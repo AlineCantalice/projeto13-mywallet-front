fb1d410bcad47a651191895d45de82cbd7d3be26
AlineCantalice <alinedecantalice@gmail.com>
2022-07-03 13:25:47 -0300

    tela de historico de entrada e saida

--- src/components/balance/Balance.js
+++ src/components/balance/Balance.js
@@ -1,5 +1,104 @@
+import styled from "styled-components";
+
 export default function Balance() {
     return (
-        <>Balance works</>
+        <Container>
+            <Header>
+                <h2>Olá, Fulano</h2>
+                <ion-icon name="log-out-outline"></ion-icon>
+            </Header>
+            <BankStatement>
+                <h3>Não há registros de entrada ou saída</h3>
+            </BankStatement>
+            <Footer>
+                <div>
+                    <ion-icon name="add-circle-outline"></ion-icon>
+                    <p>Nova entrada</p>
+                </div>
+                <div>
+                    <ion-icon name="remove-circle-outline"></ion-icon>
+                    <p>Nova saída</p>
+                </div>
+            </Footer>
+        </Container>
     );
 }
+
+const Container = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Header = styled.header`
+    width: 90vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 22px 0;
+
+    h2, ion-icon {
+        font-weight: 700;
+        font-size: 26px;
+        color: #FFFFFF;
+    }
+`
+
+const BankStatement = styled.article`
+    width: 90vw;
+    height: 65vh;
+    //margin-bottom: 110px;
+    background-color: #FFFFFF;
+    border-radius: 5px;
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+
+    p {
+        font-size: 16px;
+        color: #000000; //A cor vai depender se é data, entrada ou saida
+    }
+    
+    // Essa informação aparece quando não tem nenhum historico
+    h3 { 
+        font-size: 20px;
+        color: #868686;
+        text-align: center;
+    }
+`
+
+const Footer = styled.footer`
+    width: 90vw;
+    height: 114px;
+    display: flex;
+    justify-content: space-between;
+    position: fixed;
+    bottom: 18px;
+    left: 18px;
+
+    div {
+        width: 155px;
+        height: 114px;
+        background: #A328D6;
+        border-radius: 5px;
+        position: relative;
+    }
+
+    p {
+        font-weight: 700;
+        font-size: 17px;
+        color: #FFFFFF;
+        position: absolute;
+        left: 10px;
+        bottom: 10px;
+    }
+
+    ion-icon {
+        font-weight: 700;
+        font-size: 21px;
+        color: #FFFFFF;
+        position: absolute;
+        left: 10px;
+        top: 10px;
+    }
+`
